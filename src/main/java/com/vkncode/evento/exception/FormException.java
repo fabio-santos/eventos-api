26ecb384d7222a2df85a604d85ab84f667a7ac40
Fabio Santos <fabio.santos@soc.com.br>
2019-12-07 12:28:46 -0300

    formatando mensagens de erro de formulario

---
+++ src/main/java/com/vkncode/evento/exception/FormException.java
@@ -0,0 +1,54 @@
+package com.vkncode.evento.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class FormException {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<FormError> handle(MethodArgumentNotValidException exception) {
+		List<FormError> errorList = new ArrayList<FormError>();
+		
+		List<FieldError> formErrors = exception.getBindingResult().getFieldErrors();
+		formErrors.forEach(e -> {
+			String message = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+			errorList.add(new FormError(e.getField(), message));
+		});
+		
+		return errorList;
+	}
+	
+	public class FormError {
+		public String field;
+		public String message;
+		
+		public FormError(String field, String message) {
+			this.field = field;
+			this.message = message;
+		}
+		
+		public String getField() {
+			return field;
+		}
+		
+		public String getMessage() {
+			return message;
+		}
+	}
+}
+
